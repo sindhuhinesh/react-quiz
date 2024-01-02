da60950fce56e18265649c04837660500fb27490
Sindhu-HP <sindhumohanokmohan@gmail.com>
2024-01-02 15:42:43 +0530

    added libs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+import { fetchQuestions } from './redux/questionsSlice';
+import Start from './components/start/start.component';
+import Question from './components/question/question.component';
+import End from './components/end/end.component';
+
 import './App.css';
 
+
 function App() {
+  const dispatch = useDispatch();
+
+  useEffect( () => {
+    dispatch(fetchQuestions());
+  }, []);
+
+  const { quizQuestions } = useSelector((state) => state.quizQuestions)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h2 style={{ color: 'white', fontSize: '20px', fontWeight: 'bold' }}>{quizQuestions}</h2>
+        <Routes>
+          <Route path='/' element={ <Start /> }/>
+          <Route path='questions' element={ <Question /> }/>
+          <Route path='end' element={ <End /> }/>
+        </Routes>
+      </div>    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store/store';
+
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+
+import './index.css';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <BrowserRouter >
+        <App />
+      </BrowserRouter> 
+    </Provider>       
   </React.StrictMode>
 );
 
