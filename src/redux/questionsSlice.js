b7f6fc640438f355b1f310cd99d552a05d56bc4d
Sindhu-HP <sindhumohanokmohan@gmail.com>
2024-01-06 23:13:54 +0530

    bug fixed

--- src/redux/questionsSlice.js
+++ src/redux/questionsSlice.js
@@ -14,22 +14,49 @@ const questionsSlice = createSlice({
   name: "quizQuestions",
   initialState: {
     questionsList: [],
+    total_mark: 0,
+    selectedAnswer: ''
   },
-  reducers: {},
+
+  reducers: {
+    calculateMark: (state, action) => {
+        const optionsArray = action.payload.optionsArray;
+        const selectedAnswer = state.selectedAnswer;
+        
+        if(selectedAnswer !== '') {
+          const matchingOption = optionsArray.find(option => option.data === selectedAnswer);
+        
+          if(matchingOption.isCorrect){
+            state.total_mark = state.total_mark + 1;
+          } 
+        }
+
+        state.selectedAnswer = '';
+              
+    },
+    setSelectedAnswer: (state,action) => {
+        state.selectedAnswer = action.payload;
+    }
+  },
+
   extraReducers: (builder) => {
     builder
+
       .addCase(fetchQuestions.pending, (state, action) => {
         console.log("Pending");
       })
+
       .addCase(fetchQuestions.fulfilled, (state, action) => {
         console.log("Success");
         state.questionsList = action.payload.results;
         console.log("Success"+JSON.stringify(state.questionsList, null, 2));
       })
+
       .addCase(fetchQuestions.rejected, (state, action) => {
         console.log("Rejected");
       });
   },
 });
 
+export const { calculateMark, setSelectedAnswer } = questionsSlice.actions;
 export default questionsSlice.reducer;
